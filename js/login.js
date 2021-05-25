1b481f539c71ee1de962ff7c99115caf2d4494f3
lorenzograzzini <74548411+lorenzograzzini@users.noreply.github.com>
2021-05-25 22:05:25 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,7 +2,9 @@ function change_view() {
   var button = document.getElementById('password_input')
   if (button.getAttribute('type') == "password") {
     button.setAttribute("type","text");
+    button.setAttribute("value","Nascondi Password");
   }else if (button.getAttribute('type') == "text") {
     button.setAttribute("type","password");
+    button.setAttribute("value","Mostra Password");
   }
 }
