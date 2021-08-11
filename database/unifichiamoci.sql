81529cc8effc31b31bbc43637969eae7733aca99
lorenzograzzini <74548411+lorenzograzzini@users.noreply.github.com>
2021-08-11 13:57:50 +0200

    Update unifichiamoci.sql

--- database/unifichiamoci.sql
+++ database/unifichiamoci.sql
@@ -118,6 +118,7 @@ CREATE TABLE `member` (
   `code` varchar(10) NOT NULL,
   `name` varchar(20) NOT NULL,
   `surname` varchar(20) NOT NULL,
+  'password' varchar(20) NOT NULL,
   `picture` blob,
   PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
