54a658c37ec6d0a676e5065684771b37cd647500
lorenzograzzini <74548411+lorenzograzzini@users.noreply.github.com>
2021-08-14 12:41:33 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -8,23 +8,23 @@
   <body>
     <script src="../js/login.js" charset="utf-8"></script>
     <?php
-    if (condition) {
-      $username = htmlspecialchars("username_input")
-      $password = htmlspecialchars("password_input")
-    }
+    $username = htmlspecialchars($_GET["username_input"]);
+    //$password = htmlspecialchars($_GET["password_input"]);
+    //echo $username;
+    //echo $password;
      ?>
-    <div class="major">
-      <form method="POST">
-        <h1 class="title">Accedi</h1>
-        <h1>Username</h1>
-        <input type="text" name="username" id="username_input">
-        <h1>Password</h1>
-        <input type="password" name="Password" id="password_input">
-        <br>
-        <input type="submit" name="button_to_log" value="Accedi" class="accesso">
-      </form>
-      <input type="submit"name="password_view" value="Mostra Password" onclick="change_view();" class="viewchanger">
-    </div>
+      <div class="major">
+        <form method="GET">
+          <h1 class="title">Accedi</h1>
+          <h1>Username</h1>
+          <input type="text" name="username" id="username_input">
+          <h1>Password</h1>
+          <input type="password" name="Password" id="password_input">
+          <br>
+          <input type="submit" name="button_to_log" value="Accedi" class="accesso">
+        </form>
+        <input type="submit"name="password_view" value="Mostra Password" onclick="change_view();" class="viewchanger">
+      </div>
     <p id="error">Username o password errati.<br> Se il problema persiste, contattare l'amministratore</p>
   </body>
 </html>
